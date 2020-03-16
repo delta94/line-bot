0afb178b02380e7f05dde60cf379d05cf3aa1c5e
Tu Huynh <tu.huynh@linecorp.com>
2020-03-16 15:51:02 +0700

    first commit

---
+++ process.js
@@ -0,0 +1,95 @@
+const utils = require("./utils");
+
+let items = ["Phố Thị", "UpTown", "Cơm 27K"];
+let queues = [];
+
+// Todo: Improve Shuffle: Random Shuffle when reinit the queue is not so efficient
+// Todo: Add persistence feature: Sync / Save items and queues to hard disk
+
+function processMessage(originalMessage) {
+  const message = originalMessage.trim().toLowerCase();
+
+  if (message === "danh sách quán") {
+    const list = items.join(", ");
+    return `Danh sách quán nè: ${list}`;
+  }
+
+  if (message.startsWith("thêm quán")) {
+    const newItem = handleAddItem(message);
+    return `Đã thêm quán ${newItem}`;
+  }
+
+  if (message.startsWith("xoá quán")) {
+    const removed = handleRemoveItem(message);
+    if (removed) {
+      return `Đã xoá quán ${removed}`;
+    } else {
+      return "Không tìm thấy quán";
+    }
+  }
+
+  if (message === "ăn gì" || message === "quán nào" || message === "đổi quán") {
+    const item = handleGetFromQueue();
+    return `Mình đi ăn ${item} nha!`;
+  }
+
+  if (message === "bot") {
+    return "Dạ, bot nghe";
+  }
+
+  if (message === "lol") {
+    return "Lo code đi, chat chat cái gì?";
+  }
+
+  if (message === "haha") {
+    return "Hahahahahahahaha";
+  }
+
+  if (message.includes("bot") && /ngu|gà|chó|cc|cl|đm|đcm|cút|óc/.test(str)) {
+    return "Bot cũng biết buồn đó nha!!";
+  }
+
+  if (message.includes("bot")) {
+    return "Hello, ai gọi gì Bot dạ?";
+  }
+
+  if (message.includes("nhậu")) {
+    return "Giờ này đang mùa dịch corona, hông đi nhậu nha!!";
+  }
+
+  return null;
+}
+
+function handleAddItem(message) {
+  const itemToAdd = message.replace("thêm quán ", "");
+
+  items = [...items, itemToAdd];
+  console.log("List items: ", items);
+
+  return itemToAdd;
+}
+
+function handleRemoveItem(message) {
+  const itemToRemove = message.replace("xoá quán ", "");
+
+  items = items.filter(i => i.toLowerCase() !== itemToRemove);
+  queues = queues.filter(i => i.toLowerCase() !== itemToRemove);
+  console.log("List items: ", items);
+
+  return itemToRemove;
+}
+
+function handleGetFromQueue() {
+  // If queue is empty, re-init it
+  if (!queues.length) {
+    const clonedItems = [...items];
+    const shuffledItems = utils.shuffle(clonedItems);
+    queues = shuffledItems;
+  }
+
+  const result = queues.shift();
+
+  return result;
+}
+
+module.exports = processMessage;
---
+++ webhook-server.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const line = require("@line/bot-sdk");
+const processMessage = require("./process");
+
+const middleware = require("@line/bot-sdk").middleware;
+const JSONParseError = require("@line/bot-sdk").JSONParseError;
+const SignatureValidationFailed = require("@line/bot-sdk")
+  .SignatureValidationFailed;
+
+// Tood: Add to .env (dotenv)
+const config = {
+  channelAccessToken: process.env.ACCESS_TOKEN,
+  channelSecret: process.env.SECRET
+};
+
+const app = express();
+app.post("/webhook", middleware(config), (req, res) => {
+  Promise.all(req.body.events.map(handleEvent)).then(result =>
+    res.json(result)
+  );
+});
+
+const client = new line.Client(config);
+
+function handleEvent(event) {
+  if (event.type !== "message" || event.message.type !== "text") {
+    return Promise.resolve(null);
+  }
+
+  console.info(event.source.userId);
+  console.log(event.message.text);
+
+  const responseMessage = processMessage(event.message.text);
+
+  if (responseMessage) {
+    return client.replyMessage(event.replyToken, {
+      type: "text",
+      text: responseMessage
+    });
+  }
+
+  return Promise.resolve(null);
+}
+
+app.use((err, req, res, next) => {
+  if (err instanceof SignatureValidationFailed) {
+    res.status(401).send(err.signature);
+    return;
+  } else if (err instanceof JSONParseError) {
+    res.status(400).send(err.raw);
+    return;
+  }
+  next(err); // will throw default 500
+});
+
+app.listen(7777, () =>
+  console.log("Webhook server is listening on port 7777.")
+);
