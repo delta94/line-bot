171cbe339bf2b64a64d23a2d70ef55a9d3e1675e
Tu Huynh <tu.huynh@linecorp.com>
2020-03-17 17:43:24 +0700

    changes

--- test.js
+++
@@ -1,7 +0,0 @@
-getDurationHHmm(str) {
-    const moment = require("moment");
-    const scheduled = moment(str, "HH:mm");
-    const now = moment(new Date());
-    const duration = moment.duration(scheduled.diff(now)).asMilliseconds();
-    return duration;
-}
