2a2abaed762e9fc2ab7fe39b674fce942001db42
Tu Huynh <tu.huynh@linecorp.com>
2020-05-07 17:33:29 +0700

    Fix hard-coded [object Object] string (thanks!)

--- processors/simsimi.js
+++ processors/simsimi.js
@@ -1,4 +1,4 @@
-let key = "XXenhvc1vueZDgpW09G3B8Tyql2dE.ghSVMbLvvZ";
+let key = "keUDwl_KwDUCwqOWB~oPRBvRsKKd-Vfcjr93zLmX";
 
 async function trashTalk(text) {
   const axios = require("axios");
@@ -13,7 +13,7 @@ async function trashTalk(text) {
     const { data } = await axios.post(`https://wsapi.simsimi.com/190410/talk`, {
       utext: text,
       lang: "vi",
-      "atext_bad_prob_max": 0
+      "atext_bad_prob_max": 0.5
     }, config);
 
     return data && data.atext || "Lỗi rồi";
