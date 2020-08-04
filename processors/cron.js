3a9279fa079bdcc6ce73bed39856a61c205e6288
Tu Huynh <tu.huynh@linecorp.com>
2020-08-04 11:49:32 +0700

    better code

--- processors/cron.js
+++ processors/cron.js
@@ -2,17 +2,17 @@ const CronJob = require('cron').CronJob;
 const notify = require("../utils/notify");
 
 function executeCron() {
-  const morning = new CronJob('30 06 * * * 1-5', function() {
+  const morning = new CronJob('00 30 06 * * 1-5', function() {
     notify(`Morningg mn, chúc mọi người một ngày làm việc mới vui vẻ nha ahihi`);
   }, null, true, 'Asia/Ho_Chi_Minh');
   morning.start();
 
-  const lunch = new CronJob('50 11 * * * 1-5', function() {
+  const lunch = new CronJob('00 50 11 * * 1-5', function() {
     notify(`Trưa tới rồi, mình đi ăn trưa nha mn!!`);
   }, null, true, 'Asia/Ho_Chi_Minh');
   lunch.start();
 
-  const dinner = new CronJob('45 17 * * * 1-5', function() {
+  const dinner = new CronJob('00 45 17 * * 1-5', function() {
     notify(`Sắp 6 giờ chiều rồi nè, về đi ăn tối thôi mn!`);
   }, null, true, 'Asia/Ho_Chi_Minh');
   dinner.start();
