d14c6577ba74821d060ff3ed3b975439cb465f06
Tu Huynh <tu.huynh@linecorp.com>
2020-08-04 11:58:16 +0700

    lots of changes after a lot of time

--- processors/salary.js
+++ processors/salary.js
@@ -3,7 +3,7 @@ const moment = require("moment");
 function getSalaryDateCountDown() {
   const start = moment();
   const end = moment().endOf('month').subtract(moment().daysInMonth() === 31 ? 2 : 1, "days");
-  const days = moment.duration(end.diff(start)).asDays();
+  const days = moment.duration(end.diff(start)).asDays() - 0.5;
   return Math.round(days);
 }
 
