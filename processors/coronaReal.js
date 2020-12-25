0c89872f96f4e507bbeb9df63285df790a89f132
Tu Huynh <tu.huynh@linecorp.com>
2020-12-25 16:55:11 +0700

    pam anderson is going to love me.

--- processors/coronaReal.js
+++ processors/coronaReal.js
@@ -4,7 +4,7 @@ let currentCases = 1433;
 
 async function coronaLive() {
   const { cases, source } = await getCorona();
-  if (cases > currentCases) {
+  if (parseInt(cases) > currentCases) {
     const newCases = cases - currentCases;
     const notify = require("../utils/notify");
     notify(`Việt Nam có thêm ${newCases} ca nhiễm Corona mới mọi người ơi, tổng số cases là ${cases}!! ${source}.`);
