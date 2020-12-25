3dcf450212841e680d4d37b20ae64dd548dd9b67
Tu Huynh <tu.huynh@linecorp.com>
2020-12-25 16:56:04 +0700

    put code that worked where the code that didn't used to be

--- processors/hn.js
+++ processors/hn.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 
 async function hn() {
-  const { data } = await axios.get('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fhnrss.org%2Ffrontpage')
+  const { data } = await axios.get('http://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fhnrss.org%2Ffrontpage')
   const items = data.items
   let msg = ""
   for (const i of items) {
