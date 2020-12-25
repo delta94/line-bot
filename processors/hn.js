99a91af190341f601f097c149aaef40ef838d009
Tu Huynh <tu.huynh@linecorp.com>
2020-12-25 16:58:08 +0700

    oops

--- processors/hn.js
+++ processors/hn.js
@@ -1,12 +1,13 @@
 const axios = require('axios')
 
 async function hn() {
-  const { data } = await axios.get('http://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fhnrss.org%2Ffrontpage')
+  const { data } = await axios.get('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fhnrss.org%2Ffrontpage')
   const items = data.items
   let msg = ""
   for (const i of items) {
     msg += "-> " + i.title + "\n " + ` ${i.link}` + " \n"
   }
+  console.log(msg);
   return msg
 }
 
